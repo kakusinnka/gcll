586414f48148c9b2f3470a2e669db12a26e30180
haozhenhua <1305510482@qq.com>
2022-10-06 22:46:28 +0900

    update

---
+++ src/main/java/com/hzh/gcll/GcllApplication.java
@@ -0,0 +1,14 @@
+package com.hzh.gcll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcllApplication.class, args);
+		System.out.println("Hello Spring Boot!!!");
+	}
+
+}
---
+++ src/test/java/com/hzh/gcll/GcllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzh.gcll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
