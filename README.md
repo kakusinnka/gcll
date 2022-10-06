43c6b953d8abbd69ed0c588bb93fb573884c437d
haozhenhua <1305510482@qq.com>
2022-10-07 00:33:57 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gcll
-google-cloud-logging-logback.
+使用 google-cloud-logging-logback 写日志到cloud logging.
+
+* 创建SpringBoot工程。
+* 安装 Docker Desktop.
+* Vs Code 安装 Docker 插件。
+* mvn 打包：mvn clean package
+* 右键 Dockerfile -> build image -> [gcr.io/my-project-29437-364300/gcll:latest]
+* Ctrl + Shift + P -> Docker push -> [gcr.io/my-project-29437-364300/gcll:latest]
+* 部署到 Cloud Run
--- src/main/java/com/hzh/gcll/GcllApplication.java
+++ src/main/java/com/hzh/gcll/GcllApplication.java
@@ -1,14 +1,18 @@
 package com.hzh.gcll;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GcllApplication {
+	private static final Logger logger = LoggerFactory.getLogger(GcllApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GcllApplication.class, args);
-		System.out.println("Hello Spring Boot!!!");
+		logger.info("Logging INFO with Logback");
+		logger.error("Logging ERROR with Logback");
 	}
 
 }
